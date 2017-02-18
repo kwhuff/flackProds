d10c80b312b4b8365f54c9c79a4fe459efb52287
kwhuff <kwhuff@gmail.com>
2017-02-18 00:54:59 -0500

    test

--- global.js
+++ global.js
@@ -21,7 +21,7 @@ function goToHome(){
   window.location.href = '/'
 }
 function goToPackages(){
-  window.location.href = '/packages.html'
+  window.location.href = '/flackProds/index.html/packages.html'
 }
 function goToVendors(){
   window.location.href = '/vendors.html'
