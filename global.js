6fd085d4b2b7d11809aad169dc6449862a9ddbc5
kwhuff <kwhuff@gmail.com>
2017-02-20 13:16:55 -0500

    started contact page

--- global.js
+++ global.js
@@ -12,19 +12,24 @@ weddingButton3.addEventListener("click", displayWedding3)
 var homeLink = document.getElementById('homeLink')
 var packagesLink = document.getElementById('packagesLink')
 var vendorsLink = document.getElementById('vendorsLink')
+var contactLink = document.getElementById('contactLink')
 
 homeLink.addEventListener("click", goToHome)
 packagesLink.addEventListener("click", goToPackages)
 vendorsLink.addEventListener("click", goToVendors)
+contactLink.addEventListener("click", goToContact)
 
 function goToHome(){
-  window.location.href = '/flackProds/index.html'
+  window.location.href = '/index.html'
 }
 function goToPackages(){
-  window.location.href = '/flackProds/packages.html'
+  window.location.href = 'packages.html'
 }
 function goToVendors(){
-  window.location.href = '/flackProds/vendors.html'
+  window.location.href = 'vendors.html'
+}
+function goToContact(){
+  window.location.href = 'contact.html'
 }
 function displayWedding1(){
   $(weddingVideo1).removeClass("noDisplay")
--- packages.js
+++ packages.js
@@ -1,10 +1,12 @@
 var homeLink = document.getElementById('homeLink')
 var packagesLink = document.getElementById('packagesLink')
 var vendorsLink = document.getElementById('vendorsLink')
+var contactLink = document.getElementById('contactLink')
 
 homeLink.addEventListener("click", goToHome)
 packagesLink.addEventListener("click", goToPackages)
 vendorsLink.addEventListener("click", goToVendors)
+contactLink.addEventListener("click", goToContact)
 
 function goToHome(){
   window.location.href = 'index.html'
@@ -14,4 +16,7 @@ function goToPackages(){
 }
 function goToVendors(){
   window.location.href = 'vendors.html'
+}
+function goToContact(){
+  window.location.href = 'contact.html'
 }
