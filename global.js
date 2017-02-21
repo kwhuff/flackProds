e5adb92b0b7c3138a5508211fdcb68f0d3939afc
kwhuff <kwhuff@gmail.com>
2017-02-21 15:43:18 -0500

    adding smoothstate

--- global.js
+++ global.js
@@ -9,37 +9,6 @@ weddingButton1.addEventListener("click", displayWedding1)
 weddingButton2.addEventListener("click", displayWedding2)
 weddingButton3.addEventListener("click", displayWedding3)
 
-var homeLink = document.getElementById('homeLink')
-var packagesLink = document.getElementById('packagesLink')
-var vendorsLink = document.getElementById('vendorsLink')
-var contactLink = document.getElementById('contactLink')
-var homeLink2 = document.getElementById('homeLink2')
-var packagesLink2 = document.getElementById('packagesLink2')
-var vendorsLink2 = document.getElementById('vendorsLink2')
-var contactLink2 = document.getElementById('contactLink2')
-
-homeLink.addEventListener("click", goToHome)
-packagesLink.addEventListener("click", goToPackages)
-vendorsLink.addEventListener("click", goToVendors)
-contactLink.addEventListener("click", goToContact)
-homeLink2.addEventListener("click", goToHome)
-packagesLink2.addEventListener("click", goToPackages)
-vendorsLink2.addEventListener("click", goToVendors)
-contactLink2.addEventListener("click", goToContact)
-
-
-function goToHome(){
-  window.location.href = '/flackProds'
-}
-function goToPackages(){
-  window.location.href = 'packages.html'
-}
-function goToVendors(){
-  window.location.href = 'vendors.html'
-}
-function goToContact(){
-  window.location.href = 'contact.html'
-}
 function displayWedding1(){
   $(weddingVideo1).removeClass("noDisplay")
   $(weddingVideo2).addClass("noDisplay")
---
+++ smooth.js
@@ -0,0 +1,20 @@
+;(function ($) {
+  'use strict';
+  var content  = $('#allContent').smoothState({
+        // onStart runs as soon as link has been activated
+        onStart : {
+
+          // Set the duration of our animation
+          duration: 250,
+
+          // Alterations to the page
+          render: function () {
+
+            // Quickly toggles a class and restarts css animations
+            content.restartCSSAnimations('is-exiting');
+
+            $('body').animate({ 'scrollTop': 0 });
+          }
+        }
+      }).data('smoothState'); // makes public methods available
+})(jQuery);
