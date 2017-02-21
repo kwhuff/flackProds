c58dbe5f9a544baf8ea3862472d305e3419d106a
kwhuff <kwhuff@gmail.com>
2017-02-20 20:20:22 -0500

    routes

--- packages.js
+++ packages.js
@@ -29,7 +29,7 @@ contactLink2.addEventListener("click", goToContact)
 
 
 function goToHome(){
-  window.location.href = 'index.html'
+  window.location.href = '/'
 }
 function goToPackages(){
   window.location.href = 'packages.html'
