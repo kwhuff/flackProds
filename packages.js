02fc639a651371f98c38ffbfffcd896da9e48a2c
kwhuff <kwhuff@gmail.com>
2017-02-20 20:22:53 -0500

    routes

--- packages.js
+++ packages.js
@@ -29,7 +29,7 @@ contactLink2.addEventListener("click", goToContact)
 
 
 function goToHome(){
-  window.location.href = '/'
+  window.location.href = '/flackProds'
 }
 function goToPackages(){
   window.location.href = 'packages.html'
