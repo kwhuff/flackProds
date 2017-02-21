f4aad8ca4b9e51ea040aa33fbcc34f9770e57988
kwhuff <kwhuff@gmail.com>
2017-02-21 18:38:50 -0500

    more smoothstate stuff

--- smooth.js
+++ smooth.js
@@ -1,18 +1,14 @@
 ;(function ($) {
   'use strict';
-  var content  = $('#allContent').smoothState({
+  var content  = $('#main').smoothState({
         // onStart runs as soon as link has been activated
         onStart : {
-
           // Set the duration of our animation
-          duration: 500,
-
+          duration: 5000,
           // Alterations to the page
           render: function () {
-
             // Quickly toggles a class and restarts css animations
             content.restartCSSAnimations('is-exiting');
-
             $('body').animate({ 'scrollTop': 0 });
           }
         }
