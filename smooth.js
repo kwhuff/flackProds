520a793682506b7967c08fd74764dd38e5794528
kwhuff <kwhuff@gmail.com>
2017-02-22 16:37:53 -0500

    sliding drop down

--- smooth.js
+++ smooth.js
@@ -8,9 +8,19 @@
           // Alterations to the page
           render: function () {
             // Quickly toggles a class and restarts css animations
-            content.restartCSSAnimations('is-exiting');
-            $('body').animate({ 'scrollTop': 0 });
+            // content.restartCSSAnimations('is-exiting');
+            // $('body').animate({ 'scrollTop': 0 });
           }
         }
       }).data('smoothState'); // makes public methods available
 })(jQuery);
+
+// Add slideDown animation to Bootstrap dropdown when expanding.
+$('.dropdown').on('show.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+});
+
+// Add slideUp animation to Bootstrap dropdown when collapsing.
+$('.dropdown').on('hide.bs.dropdown', function() {
+  $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+});
